c8eab5ebabefafba59491b7091bbb4a05745128b
Victor Costa <victor.v.h.o.c@outlook.com>
2024-12-04 12:37:16 -0300

    feat: updating .lintstagedrc.js - ensuring type check
    NextJS doesn't enable type-checking when running in dev mode, so to ensure that every commit will be type-checked previously, I'm adding a line in the file to ensuring the type-checking.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,7 @@ module.exports = {
 		return [
 			`prettier --write ${filenames.join(' ')}`,
 			`next lint --fix ${filenames.map(file => `--file ${file}`).join(' ')}`,
+			'tsc --project tsconfig.json --noEmit',
 			`npm test -- --findRelatedTests ${filenames.join(' ')}`,
 		];
 	},
